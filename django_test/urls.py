c12162ec4641f029b9fdb20ea51c4aab3b75e9f5
Chris Wainner <chris.jwainner@gmail.com>
2017-08-11 09:57:22 -0700

    Update website index page, update admin details

--- django_test/urls.py
+++ django_test/urls.py
@@ -15,8 +15,10 @@ Including another URLconf
 """
 from django.conf.urls import include, url
 from django.contrib import admin
+from .views import index
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^music/', include('music.urls')),
+    url(r'^$', index)
 ]
