1fd1cce71b53767649fa6a7815b5e8d2bf9a1c87
Chris Wainner <chris.jwainner@gmail.com>
2017-08-30 10:52:00 -0700

    Add album links to music page, fix album detail url

--- music/urls.py
+++ music/urls.py
@@ -5,5 +5,5 @@ urlpatterns = [
   # Homepage
   url(r'^$', views.index, name='index'),
   # /music/album-id
-  url(r'^(?P<album_id>[0-9]+)$', views.detail)
+  url(r'^(?P<album_id>[0-9]+)/$', views.detail)
 ]
